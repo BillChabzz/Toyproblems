2572ebb98c4d213d2fb58b6b3d30f8864c85b67b
BillChabzz <chaby9.8@gmail.com>
2015-09-08 10:32:45 +0300

    Program to print text using a constructor

---
+++ Msprint.java
@@ -0,0 +1,21 @@
+
+public class Msprint {
+	Msprint(){
+		//String string = "Welcome to Moringa school";
+		System.out.println("******************");
+		System.out.println("**              **");
+		System.out.println("** Welcome to   **");
+		System.out.println("**   Moringa    **");
+		System.out.println("**   School     **");
+		System.out.println("******************");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Msprint b = new Msprint();
+	}
+
+}

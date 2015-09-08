8f7bdb9a3b0449b050ca1d4aa4a9950dc386b176
BillChabzz <chaby9.8@gmail.com>
2015-09-08 10:05:16 +0300

    Java program that asks for input and displays welcome text

---
+++ Requestname.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Requestname {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please enter your name");
+		Scanner name = new Scanner(System.in);
+		System.out.println(name.nextLine() + " ,welcome to moringa school!");
+
+	}
+
+}
